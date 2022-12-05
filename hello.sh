db04fdc54763a5dcd83fe10abfafa0ce11e9a0ac
Khalid Hossen <52601910+Khalid019@users.noreply.github.com>
2022-12-05 19:05:13 +0600

    Hello world

---
+++ hello.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "Hello world"
+
+# This is a comment alawys start with #
+# variables
+
+## System variables
+echo My shell name $BASH
+echo My home directory $HOME
+echo My shell version is $BASH_VERSION
+echo My current directory is $PWD
+
+## User defined variables
+name=Mark
+echo The name is $name
+val=10
+echo The value is $val
